c4072b772331e682eaafff1f59f6f3ecd5b6ed10
bAngerman <Brendan.angerman@gmail.com>
2017-11-15 20:25:23 -0700

    jasmine expect set

--- js/models/course.js
+++ js/models/course.js
@@ -4,20 +4,36 @@
 
 		Course = Backbone.Model.extend({
 			defaults: {
-				name: ''
+				name: '',
+				code: '',
+				instructor: '',
+				classes: []
+
 				// TODO: include remaining defaults
 			},
 
 			// TODO: include a funtion to add class times (e.g. addClassTime(time))
 
 			validate: function(attrs) {
-				// TODO: modify this validation to suit your needs
-			    if (attrs.hasOwnProperty('name') && _.isEmpty(attrs.name)) {
-			        return 'Course name cannot be empty.';
-			    }
+				
+				var errors = []; // holds all of the validationErrors
 
-			    // TODO: complete validation for the remaining attributes
-			    // classes can be a bit 'tricky'
+			  if (attrs.hasOwnProperty('name') && _.isEmpty(attrs.name)) {
+					errors.push({name: 'emptyName', message: 'Course name cannot be empty.'});
+				}
+
+				if (attrs.hasOwnProperty('code') && _.isEmpty(attrs.code)) {
+					errors.push({name: 'emptyCode', message: 'Course code cannot be empty.'});
+				}
+
+				if (attrs.hasOwnProperty('instructor') && _.isEmpty(attrs.instructor)) {
+					errors.push({name: 'emptyInstructor', message: 'Instructor\'s name cannot be empty.'});
+				} 
+
+			  // TODO: complete validation for the remaining attributes
+				// classes can be a bit 'tricky'
+				
+				return errors.length > 0 ? errors: false; // if there are errors, return them. if there are none then return false
 			}
 		});
 
--- test/models/test.course.js
+++ test/models/test.course.js
@@ -15,25 +15,49 @@ describe('Course', function() {
 	});
 
 	describe('validates set attributes', function() {
+		
 		it('ensures a non-empty name is provided', function() {
-			var errorCallback = jasmine.createSpy('-invalid event callback-');
+
+			// ---------- This is Nathan's in class way -------------
+			// var errorCallback = jasmine.createSpy('-invalid event callback-');
 			
-			course.on('invalid', errorCallback);
+			// course.on('invalid', errorCallback);
 
-		    // What would you need to set on the course properties to
-		    // cause validation to fail?
+		    // // What would you need to set on the course properties to
+		    // // cause validation to fail?
 
-		    course.set({name:''}, {validate: true}); // cannot be empty!
+		    // course.set({name:''}, {validate: true}); // cannot be empty!
 
-		    var errorArgs = errorCallback.calls.mostRecent().args;
+		    // var errorArgs = errorCallback.calls.mostRecent().args;
 
-		    expect(errorArgs).toBeDefined();
-		    expect(errorArgs[0]).toBe(course);
-		    expect(errorArgs[1]).toBe('Course name cannot be empty.');
+		    // expect(errorArgs).toBeDefined();
+		    // expect(errorArgs[0]).toBe(course);
+			// expect(errorArgs[1]).toBe('Course name cannot be empty.');
+			// -------------------------------------------------------
+
+			// empty name
+			course.set({name: ''}, {validate: true}); 
+
+			// course.validationError is a list of errors sent from the course's validate function
+			// _.find is an undescores function that takes in a list, and a predicate ( _.find(list, predicate) )
+			// I reason that the underscore dependancy is okay because the Backbone.js .set function itself relies on underscore.
+			// the predicate to check whether the error exist is a function that returns true of false based on whether the course exsts.
+    	expect(_.find(course.validationError, function(err) { return err.name === 'emptyName'; } ).message).toEqual('Course name cannot be empty.');
 		});
 
 		// TODO: create validation tests for code, instructor, and classes
 
+		it('ensures a non-empty code is provided', function() {
+			course.set({code: ''}, {validate: true}); // empty code
+			expect(_.find(course.validationError, function(err) { return err.name === 'emptyCode'; } ).message).toEqual('Course code cannot be empty.');
+		});
+
+		it('ensures a non-empty instructor is provided', function() {
+			course.set({instructor: ''}, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'emptyInstructor'; } ).message).toEqual('Instructor\'s name cannot be empty.');			
+		});
+
 		// TODO: create specific tests for classes -> day, start, and end times
 	});
+
 });

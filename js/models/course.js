e628c774c749d94dc89e5b5d366a008f13bce5be
bAngerman <Brendan.angerman@gmail.com>
2017-11-16 13:30:56 -0700

    Course tests + validation

--- js/models/course.js
+++ js/models/course.js
@@ -17,10 +17,117 @@
 			validate: function(attrs) {
 				
 				var errors = []; // holds all of the validationErrors
+				var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+				/**
+				 * Helper function to reduce repitition when validating for classes' day, start, and end key existence.
+				 * 
+				 * @param {string} type - either 'day', 'start', or 'end'.
+				 */
+				function classesKeyMissing(type) {
+					var missing = false;
+					attrs.classes.forEach(function(row) {
+						if (!_.has(row, type)) {
+							missing = true;
+						}
+					});
+					return missing;
+				}
+				
+				/**
+				 * Checks if course day is non-empty, and valid.
+				 */
+				function invalidClassDay() {
+					var invalid = false;
+					attrs.classes.forEach(function(row) {
+						if (typeof row.day !== 'undefined') {
+							if (_.indexOf(days, row.day.toLowerCase()) === -1) {
+								invalid = true;
+							}
+						}
+					});
+					return invalid;
+				}
+
+				/**
+				 * Checks if course times are both non-empty and correctly formatted.
+				 * 
+				 * @param {string} type - either 'start' or 'end'
+				 */
+				function invalidClassTime(type) {
+					var fmt = new RegExp("^((10|11|12|[1-9]):[0-5][0-9][A|P][M])$");
+					var invalid = false;
+					attrs.classes.forEach(function(row) {
+						if (type == 'start') {
+							if (!fmt.test(row.start)) { 
+								invalid = true;
+							}
+						}
+						else if (type == 'end') {
+							if (!fmt.test(row.end)) {
+								invalid = true;
+							}
+						}	
+					});
+					return invalid;
+				}
+
+				/**
+				 * Change 12hr format to 24hr format time for easier comparison.
+				 * 
+				 * @param {string} time - 12hr format
+				 */
+				function timeFormat(time) {
+					var hrs,
+							formattedTime = [];
+					time = time.split(':');
+					hrs = parseInt(time[0]);
+					if ((time[1].indexOf('AM') != -1 && hrs != 12) || (time[1].indexOf('PM') != -1 && hrs == 12)) { // 1AM -> 12:59PM
+						formattedTime.hrs = hrs;
+						formattedTime.mins = parseInt(time[1]);
+					}
+					else if (time[1].indexOf('PM') != -1 && hrs != 12) { // 1PM -> 11:59PM
+						formattedTime.hrs = hrs + 12;
+						formattedTime.mins = parseInt(time[1]);
+					}
+					else if (time[1].indexOf('AM') != -1 && hrs == 12) { // 12AM
+						formattedTime.hrs = hrs - 12;
+						formattedTime.mins = parseInt(time[1]);
+					}
+					return formattedTime;
+				}
+
+				/**
+				 * Determines whether end time occurs before start time in a class.
+				 * NOTE: This does not compare properly if the class starts at night, and ends the next morning. Which probably wont happen anyways.
+				 */
+				function invalidEndBeforeStart() {
+					var	invalid = false,
+							startTime,
+							endTime;
+					attrs.classes.forEach(function(row) {
+						if (typeof row.start != 'undefined' && typeof row.end != 'undefined') {
+							startTime = timeFormat(row.start);
+							endTime = timeFormat(row.end);
+							// console.log(startTime);
+							// console.log(endTime);
+							if (startTime.hrs > endTime.hrs) {
+								invalid = true;
+							}
+							else if (startTime.hrs == endTime.hrs && startTime.mins > endTime.mins) { //same hr, but end minutes are earlier
+								invalid = true;
+							}
+						}
+					});
+					return invalid;
+				}
 
 			  if (attrs.hasOwnProperty('name') && _.isEmpty(attrs.name)) {
 					errors.push({name: 'emptyName', message: 'Course name cannot be empty.'});
 				}
+
+				// TODO: complete validation for the remaining attributes
+				// classes can be a bit 'tricky'
 
 				if (attrs.hasOwnProperty('code') && _.isEmpty(attrs.code)) {
 					errors.push({name: 'emptyCode', message: 'Course code cannot be empty.'});
@@ -28,10 +135,43 @@
 
 				if (attrs.hasOwnProperty('instructor') && _.isEmpty(attrs.instructor)) {
 					errors.push({name: 'emptyInstructor', message: 'Instructor\'s name cannot be empty.'});
-				} 
+				}
 
-			  // TODO: complete validation for the remaining attributes
-				// classes can be a bit 'tricky'
+				if (attrs.hasOwnProperty('classes') && _.isEmpty(attrs.classes)) {
+					errors.push({name: 'emptyCourseList', message: 'Provide at least one course.'});
+				}
+
+				if (attrs.hasOwnProperty('classes') && classesKeyMissing('day')) {
+					errors.push({name: 'missingClassDay', message: 'Class day is missing.'});						
+				}
+
+				if(attrs.hasOwnProperty('classes') && invalidClassDay()) {
+					errors.push({name: 'invalidClassDay', message: 'Class day cannot be empty or is invalid.'});											
+				}
+
+				if (attrs.hasOwnProperty('classes') && classesKeyMissing('start')) {
+					errors.push({name: 'missingClassStart', message: 'Class start time is missing.'});						
+				}
+
+				if (attrs.hasOwnProperty('classes') && invalidClassTime('start')) {
+					errors.push({name: 'invalidClassStart', message: 'Class start time is invalid.'});						
+				}
+
+				if (attrs.hasOwnProperty('classes') && classesKeyMissing('end')) {
+					errors.push({name: 'missingClassEnd', message: 'Class end time is missing.'});											
+				}
+
+				if (attrs.hasOwnProperty('classes') && invalidClassTime('end')) {
+					errors.push({name: 'invalidClassEnd', message: 'Class end time is invalid.'});						
+				}
+
+				if (attrs.hasOwnProperty('classes') && !invalidClassTime('start') && !invalidClassTime('end')) { // validate times before attempting to compare them
+					if (invalidEndBeforeStart()) {
+						errors.push({name: 'classStartEndConflict', message: 'Class end time cannot occur before start time.'});						
+					}
+				}
+
+				//console.log(errors);
 				
 				return errors.length > 0 ? errors: false; // if there are errors, return them. if there are none then return false
 			}
--- test/models/test.course.js
+++ test/models/test.course.js
@@ -35,11 +35,10 @@ describe('Course', function() {
 			// expect(errorArgs[1]).toBe('Course name cannot be empty.');
 			// -------------------------------------------------------
 
-			// empty name
 			course.set({name: ''}, {validate: true}); 
 
 			// course.validationError is a list of errors sent from the course's validate function
-			// _.find is an undescores function that takes in a list, and a predicate ( _.find(list, predicate) )
+			// _.find is an underscore.js function that takes in a list, and a predicate ( _.find(list, predicate) )
 			// I reason that the underscore dependancy is okay because the Backbone.js .set function itself relies on underscore.
 			// the predicate to check whether the error exist is a function that returns true of false based on whether the course exsts.
     	expect(_.find(course.validationError, function(err) { return err.name === 'emptyName'; } ).message).toEqual('Course name cannot be empty.');
@@ -55,6 +54,49 @@ describe('Course', function() {
 		it('ensures a non-empty instructor is provided', function() {
 			course.set({instructor: ''}, {validate: true});
 			expect(_.find(course.validationError, function(err) { return err.name === 'emptyInstructor'; } ).message).toEqual('Instructor\'s name cannot be empty.');			
+		});
+
+		it('ensures at least one class is provided', function() {
+			course.set({classes: []}, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'emptyCourseList'; } ).message).toEqual('Provide at least one course.');
+		});
+
+		// copy pasta
+		// classes: [{ day: 'Monday', start: '8:00AM', end: '10:00AM' }, { day: 'Thursday', start: '8:00AM', end: '10:00AM' }]
+
+		it('ensures a course day is provided for each class', function() {
+			course.set({ classes: [{start: '8:00AM', end: '10:00AM'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'missingClassDay'; } ).message).toEqual('Class day is missing.');			
+		});
+
+		it('ensures course day is a valid day of the week (and therefore non-empty)', function() {
+			course.set({ classes: [{day: 'Hamburger', start: '8:00AM', end: '10:00AM'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'invalidClassDay'; } ).message).toEqual('Class day cannot be empty or is invalid.');
+		});
+
+		it ('ensures start time is provided for each class', function() {
+			course.set({ classes: [{day: 'Monday', end: '10:00AM'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'missingClassStart'; } ).message).toEqual('Class start time is missing.');						
+		});
+
+		it('ensures course start time is valid (and therefore non-empty)', function() {
+			course.set({ classes: [{day: 'Monday', start: '9:00', end: '10:00AM'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'invalidClassStart'; } ).message).toEqual('Class start time is invalid.');
+		});
+
+		it ('ensures end time is provided for each class', function() {
+			course.set({ classes: [{day: 'Monday', start: '9:00AM'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'missingClassEnd'; } ).message).toEqual('Class end time is missing.');						
+		});
+
+		it('ensures course end time is valid (and therefore non-empty)', function() {
+			course.set({ classes: [{day: 'Tuesday', start: '3:00PM', end: '1:asd'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'invalidClassEnd'; } ).message).toEqual('Class end time is invalid.');
+		});
+
+		it('ensures course end time does not occur before start time', function() {
+			course.set({ classes: [{day: 'Tuesday', start: '11:00AM', end: '9:00AM'}] }, {validate: true});
+			expect(_.find(course.validationError, function(err) { return err.name === 'classStartEndConflict'; } ).message).toEqual('Class end time cannot occur before start time.');
 		});
 
 		// TODO: create specific tests for classes -> day, start, and end times

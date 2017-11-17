6fadc82eeeced80ec7a22397c2380498abd0b8e9
mbublitz1 <mbublitz1@studentmail.nait.ca>
2017-11-16 19:02:54 -0700

    Filter functions for schedule

--- js/collections/schedule.js
+++ js/collections/schedule.js
@@ -16,9 +16,37 @@
 
                     return courseCheck;
                 });
-            }
-
+            },
             // TODO: complete remaining filter functions (see test.schedule.js)
+            forCode: function(code){
+                return this.filter(function(course){
+                    var codeCheck;
+                    if(course.get('code').toUpperCase() === code.toUpperCase()){
+                        codeCheck = true;
+                    }
+                    return codeCheck;
+                });
+            },
+
+            forName: function (name) {
+                return this.filter(function (course) {
+                    var nameCheck;
+                    if(course.get('name').toUpperCase() === name.toUpperCase()){
+                        nameCheck = true;
+                    }
+                    return nameCheck;
+                });
+            },
+
+            forInstructor: function (instructor) {
+                return this.filter(function(course){
+                   var instructorCheck;
+                   if(course.get('instructor').toUpperCase() === instructor.toUpperCase()){
+                       instructorCheck = true;
+                   }
+                   return instructorCheck;
+                });
+            }
         });
 
     // export the Schedule collection
--- test/collections/test.schedule.js
+++ test/collections/test.schedule.js
@@ -3,6 +3,11 @@ describe('Schedule', function() {
 
     beforeEach(function() {
         schedule = new app.collections.Schedule();
+        schedule.add([
+            { code: 'COMP1000', name: 'Course 1', instructor: 'Jane Doe', classes: [{ day: 'Monday', start: '8:00AM', end: '10:00AM' }, { day: 'Thursday', start: '8:00AM', end: '10:00AM' }] },
+            { code: 'COMP1001', name: 'Course 2', instructor: 'Jane Doe', classes: [{ day: 'Monday', start: '12:00PM', end: '2:00PM' }] },
+            { code: 'COMP1002', name: 'Course 3', instructor: 'Jon Doe', classes: [{ day: 'Wednesday', start: '1:00PM', end: '3:00PM' }] }
+        ]);
     });
 
     describe('Allows for adding courses', function() {
@@ -13,20 +18,28 @@ describe('Schedule', function() {
 
     describe('Provides filters', function() {
         describe('Filters courses by day of week', function() {
-            beforeEach(function() {
-            	// Add some courses to test the filter functionality of the collection
-                schedule.add([
-                    { code: 'COMP1000', name: 'Course 1', instructor: 'Jane Doe', classes: [{ day: 'Monday', start: '8:00AM', end: '10:00AM' }, { day: 'Thursday', start: '8:00AM', end: '10:00AM' }] },
-                    { code: 'COMP1001', name: 'Course 2', instructor: 'Jane Doe', classes: [{ day: 'Monday', start: '12:00PM', end: '2:00PM' }] },
-                    { code: 'COMP1002', name: 'Course 3', instructor: 'Jon Doe', classes: [{ day: 'Wednesday', start: '1:00PM', end: '3:00PM' }] }
-                ]);
-            });
-
             it('Returns only courses with classes on a desired day', function() {
                 expect(schedule.forDay('monday').length).toEqual(2);
                 expect(schedule.forDay('tuesday').length).toEqual(0);
             });
         });
         // TODO: complete tests for filter by code, name, and instructor (name these functions appropriately)
+        describe('Filters course by code', function () {
+            it('Returns only courses with the specified code', function () {
+                expect(schedule.forCode('COMP1002').length).toEqual(1);
+            });
+        });
+
+        describe('Filters courses by name', function () {
+            it('Returns only courses with the specified name', function () {
+                expect(schedule.forName('Course 2').length).toEqual(1);
+            });
+        });
+
+        describe('Filters course by instructor', function () {
+            it('Returns only courses with the specified instructor', function () {
+                expect(schedule.forInstructor('Jane Doe').length).toEqual(2);
+            });
+        });
     });
 });
